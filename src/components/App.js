fc668473a1ad2fc041031aff064203d5b7972d91
nkutete <nathankutete99@gmail.com>
2024-09-11 14:35:26 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SearchButton from './SearchButton';
 import Map from './Map';
-import './styles/style.css';
+import '../styles/style.css';
 
 function App() {
   return (
