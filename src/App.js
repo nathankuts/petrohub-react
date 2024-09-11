cfa70b81cef2dc0a95e3b10154575fe4827d39cc
nkutete <nathankutete99@gmail.com>
2024-09-11 13:02:36 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SearchButton from './SearchButton';
+import Map from './Map';
+import './styles/style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div id="app">
+        <Routes>
+          <Route path="/" element={<SearchButton />} />
+          <Route path="/map" element={<Map />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
