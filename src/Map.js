e5a5fb007c742a4a5465351bb3cc838222e135ea
nkutete <nathankutete99@gmail.com>
2024-09-11 14:11:14 +0300

    Update Map.js

--- src/Map.js
+++ src/Map.js
@@ -15,13 +15,13 @@ const Map = () => {
     ];
 
     gasStations.forEach(station => {
-      const popupContent = `<div class="popup-content"><b>${station.name}</b><br>`;
+      let popupContent = `<div class="popup-content"><b>${station.name}</b><br>`;
       station.fuel.forEach(fuel => {
         popupContent += `Fuel Type: <span class="fuel-type">${fuel.type}</span><br>Price: KES <span class="price">${fuel.price}</span><br>`;
       });
       popupContent += `<a href="gas_station_details_${station.name.replace(/\s+/g, '_').toLowerCase()}.html">More Details</a></div>`;
       
-      const marker = L.marker([station.latitude, station.longitude]).addTo(map).bindPopup(popupContent);
+      let marker = L.marker([station.latitude, station.longitude]).addTo(map).bindPopup(popupContent);
       marker._icon.classList.add('blink');
     });
 
