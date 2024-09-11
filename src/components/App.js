29effc3b4eb24cc759ff4413447240b91e344b29
nkutete <nathankutete99@gmail.com>
2024-09-11 15:03:14 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styles/style.css';  // Import main styles
+import '../styles/style.css';  // Import main styles
 import SearchButton from './components/SearchButton';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Map from './components/Map';
