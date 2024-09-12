d26a2617aa36384a9ea6c0de7766f41061e583ab
nkutete <nathankutete99@gmail.com>
2024-09-12 14:15:37 +0300

    Update GasStationMarker.js

--- src/components/GasStationMarker.js
+++ src/components/GasStationMarker.js
@@ -1,27 +1,17 @@
+import React from 'react';
 import L from 'leaflet';
-import { Marker, Popup } from "react-leaflet";
 
-
-const GasStationMarker = ({ station, map }) => {
+const GasStationMarker = ({ station }) => {
   useEffect(() => {
-    if (!map) return; // Prevent executing if the map object is not available yet
-
-    let popupContent = `<div class="popup-content"><b>${station.name}</b><br>`;
+    const popupContent = `<div class="popup-content"><b>${station.name}</b><br>`;
     station.fuel.forEach(fuel => {
       popupContent += `Fuel Type: <span class="fuel-type">${fuel.type}</span><br>Price: KES <span class="price">${fuel.price}</span><br>`;
     });
     popupContent += `<a href="gas_station_details_${station.name.replace(/\s+/g, '_').toLowerCase()}.html">More Details</a></div>`;
-
-    let marker = L.marker([station.latitude, station.longitude])
-      .addTo(map)
-      .bindPopup(popupContent);
+    
+    const marker = L.marker([station.latitude, station.longitude]).addTo(map).bindPopup(popupContent);
     marker._icon.classList.add('blink');
-
-    // Cleanup on unmount
-    return () => {
-      map.removeLayer(marker);
-    };
-  }, [station, map]); // Trigger when station or map changes
+  }, [station]);
 
   return null;
 };
