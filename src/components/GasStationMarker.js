046540c1846e1d180fc1cef13523280558afcc40
nkutete <nathankutete99@gmail.com>
2024-09-12 13:47:30 +0300

    Update GasStationMarker.js

--- src/components/GasStationMarker.js
+++ src/components/GasStationMarker.js
@@ -2,17 +2,27 @@ import React from 'react';
 import L from 'leaflet';
 import React, { useEffect } from 'react';
 
+
 const GasStationMarker = ({ station, map }) => {
   useEffect(() => {
+    if (!map) return; // Prevent executing if the map object is not available yet
+
     const popupContent = `<div class="popup-content"><b>${station.name}</b><br>`;
     station.fuel.forEach(fuel => {
       popupContent += `Fuel Type: <span class="fuel-type">${fuel.type}</span><br>Price: KES <span class="price">${fuel.price}</span><br>`;
     });
     popupContent += `<a href="gas_station_details_${station.name.replace(/\s+/g, '_').toLowerCase()}.html">More Details</a></div>`;
-    
-    const marker = L.marker([station.latitude, station.longitude]).addTo(map).bindPopup(popupContent);
+
+    const marker = L.marker([station.latitude, station.longitude])
+      .addTo(map)
+      .bindPopup(popupContent);
     marker._icon.classList.add('blink');
-  }, [station, map]);
+
+    // Cleanup on unmount
+    return () => {
+      map.removeLayer(marker);
+    };
+  }, [station, map]); // Trigger when station or map changes
 
   return null;
 };
