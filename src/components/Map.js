254eee33ae40c236b24c81e6f37036304c8507d6
nkutete <nathankutete99@gmail.com>
2024-09-12 12:56:10 +0300

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import React, { useEffect } from 'react';
-import L from 'leaflet';
+//import L from 'leaflet';
 import "leaflet/dist/leaflet.css";
 import '../styles/map.css';
 
