a5bad94118b14ee697e7bdab1e72597334adc9fb
nkutete <nathankutete99@gmail.com>
2024-09-12 14:01:16 +0300

    Update GasStationMarker.js

--- src/components/GasStationMarker.js
+++ src/components/GasStationMarker.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from "react"
 import L from 'leaflet';
 
 
@@ -6,13 +6,13 @@ const GasStationMarker = ({ station, map }) => {
   useEffect(() => {
     if (!map) return; // Prevent executing if the map object is not available yet
 
-    const popupContent = `<div class="popup-content"><b>${station.name}</b><br>`;
+    let popupContent = `<div class="popup-content"><b>${station.name}</b><br>`;
     station.fuel.forEach(fuel => {
       popupContent += `Fuel Type: <span class="fuel-type">${fuel.type}</span><br>Price: KES <span class="price">${fuel.price}</span><br>`;
     });
     popupContent += `<a href="gas_station_details_${station.name.replace(/\s+/g, '_').toLowerCase()}.html">More Details</a></div>`;
 
-    const marker = L.marker([station.latitude, station.longitude])
+    let marker = L.marker([station.latitude, station.longitude])
       .addTo(map)
       .bindPopup(popupContent);
     marker._icon.classList.add('blink');
