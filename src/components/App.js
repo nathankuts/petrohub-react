5b357428f501f9cf1a654a82f258df28e07694c1
nkutete <nathankutete99@gmail.com>
2024-09-13 20:54:53 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SearchButton from './SearchButton';
 import Map from './Map';
 import '../styles/styles.css';
-<<<<<<< HEAD
-
-=======
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
->>>>>>> e5e1e008169e1150a7e48be4dad3b27f1e249cf2
 
 function App() {
   return (
