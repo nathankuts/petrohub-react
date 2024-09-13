da0f5d11c552f59c8464bd5abf3bcd5048f90062
nkutete <nathankutete99@gmail.com>
2024-09-13 20:58:29 +0300

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,51 +1,46 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import React, { useEffect } from 'react';
+import L from 'leaflet';
 import './styles/map.css';
 import './styles/styles.css';
 
-const gasStations = [
-  {
-    name: "Kipetro Gas Station",
-    fuel: [{ type: "Petrol", price: 150 }],
-    latitude: 1.0151,
-    longitude: 35.0077,
-  },
-  // Add more gas stations here as needed
-];
-
 const Map = () => {
-  return (
-    <MapContainer
-      center={[1.0151, 35.0077]}
-      zoom={10}
-      style={{ height: '100vh', width: '100%' }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+  useEffect(() => {
+    const map = L.map('map').setView([1.0151, 35.0077], 10);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    const gasStations = [
+      {
+        name: "Kipetro Gas Station",
+        fuel: [{ type: "Petrol", price: 150 }],
+        latitude: 1.0151,
+        longitude: 35.0077,
+      },
       
-      {gasStations.map((station, index) => (
-        <Marker key={index} position={[station.latitude, station.longitude]}>
-          <Popup>
-            <div className="popup-content">
-              <b>{station.name}</b><br />
-              {station.fuel.map((fuel, idx) => (
-                <div key={idx}>
-                  Fuel Type: <span className="fuel-type">{fuel.type}</span><br />
-                  Price: KES <span className="price">{fuel.price}</span><br />
-                </div>
-              ))}
-              <a href={`gas_station_details_${station.name.replace(/\s+/g, '_').toLowerCase()}.html`}>
-                More Details
-              </a>
-            </div>
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
-  );
+      // Add your gas stations here, as in map.js
+    ];
+
+    gasStations.forEach(station => {
+      let popupContent = <div class="popup-content"><b>${station.name}</b><br>;
+      station.fuel.forEach(fuel => {
+        popupContent += Fuel Type: <span class="fuel-type">${fuel.type}</span><br>Price: KES <span class="price">${fuel.price}</span><br>;
+      });
+      popupContent += <a href="gas_station_details_${station.name.replace(/\s+/g, '_').toLowerCase()}.html">More Details</a></div>;
+      
+      const marker = L.marker([station.latitude, station.longitude]).addTo(map).bindPopup(popupContent);
+      marker._icon.classList.add('blink');
+    });
+
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.get('focus') === 'kitale') {
+      map.setView([1.0151, 35.0077], 12);
+    }
+    map.invalidateSize();
+  }, []);
+
+  return <div id="map"></div>;
 };
 
 export default Map;
