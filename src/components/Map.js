230cf4516ab4521df6637ab9d5ec2c8104601cfe
nkutete <nathankutete99@gmail.com>
2024-09-14 13:26:28 +0300

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,50 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import L from 'leaflet';
+import GasStationMarker from './GasStationMarker'; // Import marker component
 import './styles/map.css';
 
 const Map = () => {
+  const [map, setMap] = useState(null);
+
   useEffect(() => {
-    const map = L.map('map').setView([1.0151, 35.0077], 10);
+    const mapInstance = L.map('map').setView([1.0151, 35.0077], 10);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    }).addTo(mapInstance);
 
-    const gasStations = [
-      {
-        name: "Total",
-        fuel: [{ type: "Petrol", price: 150 }, {type: "Diesel", price: 125 }],
-        latitude: 1.0123,
-        longitude: 35.0112
-      },
-      // Add more stations here
-    ];
-
-    gasStations.forEach(station => {
-      let popupContent = `
-        <div class="popup-content">
-          <b>${station.name}</b><br>
-          ${station.fuel.map(fuel => `
-            Fuel Type: <span class="fuel-type">${fuel.type}</span><br>
-            Price: KES <span class="price">${fuel.price}</span><br>
-          `).join('')}
-          <a href="gas_station_details_${station.name.replace(/\s+/g, '_').toLowerCase()}.html">More Details</a>
-        </div>
-      `;
-
-      const marker = L.marker([station.latitude, station.longitude])
-        .addTo(map)
-        .bindPopup(popupContent);
-      marker._icon.classList.add('blink');
-    });
+    setMap(mapInstance);
 
     const urlParams = new URLSearchParams(window.location.search);
     if (urlParams.get('focus') === 'kitale') {
-      map.setView([1.0151, 35.0077], 12);
+      mapInstance.setView([1.0151, 35.0077], 12);
     }
   }, []);
 
-  return <div id="map" style={{ height: "100vh" }}></div>;
+  const gasStations = [
+    {
+      name: "Total",
+      fuel: [{ type: "Petrol", price: 150 }, { type: "Diesel", price: 125 }],
+      latitude: 1.0123,
+      longitude: 35.0112
+    },
+    // Add more stations here
+  ];
+
+  return (
+    <div id="map" style={{ height: "100vh" }}>
+      {map && gasStations.map(station => (
+        <GasStationMarker key={station.name} station={station} map={map} />
+      ))}
+    </div>
+  );
 };
 
 export default Map;
