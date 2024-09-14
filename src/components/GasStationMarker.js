0f3c3169c308e74b172c49349edad024d8c45199
nkutete <nathankutete99@gmail.com>
2024-09-14 14:07:34 +0300

    Update GasStationMarker.js

--- src/components/GasStationMarker.js
+++ src/components/GasStationMarker.js
@@ -18,14 +18,4 @@ const GasStationMarker = ({ station }) => {
   return null;
 };
 
-// Fix for default marker icon not showing
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
-
-
 export default GasStationMarker;
