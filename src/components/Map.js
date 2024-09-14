f4609f54299c98920fa80db52b3c55ca58ebebda
nkutete <nathankutete99@gmail.com>
2024-09-14 13:55:02 +0300

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import L from 'leaflet';
-import GasStationMarker from './GasStationMarker';
 import './styles/map.css';
+import 'leaflet/dist/leaflet.css';
 
 const Map = () => {
   useEffect(() => {
