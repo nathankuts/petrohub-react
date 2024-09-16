2da2ed6f9137cf9b7093e89620d8d56fb9288975
nkutete <nathankutete99@gmail.com>
2024-09-16 14:38:33 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SearchButton from './SearchButton';
 import Map from './Map';
 import '../styles/styles.css';
-import "leaflet/dist/leaflet.css";
 
 function App() {
   return (
